6403f43d6a9987d96b9213e827d59058682bd68b
Warre-Demey <Warre.Demey@student.tsaam.be>
2024-09-13 10:18:21 +0200

    opplossing opgave optellen

---
+++ optellen.js
@@ -0,0 +1,14 @@
+const optellen = (getal1, getal2) => {
+  return getal1 + getal2;
+};
+
+const aftrekken = (getal1, getal2) => {
+  return getal1 - getal2;
+};
+
+//hier moet ik zeggen welke functies de exteren partner mag / kan gebruiken
+
+module.exports = {
+  optellen,
+  aftrekken,
+};
--- useOptellen.js
+++ useOptellen.js
@@ -1,3 +1,11 @@
-const rekenen = require("./optellen");
+// const rekenen = require("./optellen");
+
+const rekenen = require("./optellen"); // de inhoud van mijn ander bestand
+
+// rekenen stel je gelijk aan de functies in /optellen.js
+
+// envoor de specifieke functie gebruiken zet je een . met daarna de functie
+//bv. rekenen . optellen
 
 console.log(rekenen.optellen(2, 3));
+console.log(rekenen.aftrekken(2, 3));
